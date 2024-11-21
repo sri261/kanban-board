7abfe7d9f6e271bb7ade4a825cfce7a2236f7e94
Sriharsh Dubale <dsriharsh@gmail.com>
2024-11-20 19:22:50 -0500

    delete button alignment fix

--- src/components/Column.vue
+++ src/components/Column.vue
@@ -38,18 +38,18 @@ const onBlur = () => {
 
 <template>
     <div class="flex flex-col bg-gray-100 rounded-lg p-4 min-w-[250px] mt-4">
-        <div class="flex justify-between">
+        <div class="flex justify-between content-between">
             <input v-if="showEditTitle" type="text"
                 class="form-input block w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                 v-model="editTitle" @blur="onBlur" />
-            <h2 v-else="!showEditTitle" class="font-bold text-xl text-gray-700 mb-4" @click="onTitleClick">{{ title }}
+            <h2 v-else="!showEditTitle" class="font-bold text-xl text-gray-700" @click="onTitleClick">{{ title }}
             </h2>
             <button @click="onDeleteClick"
-                class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+                class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold px-2 rounded inline-flex items-center">
                 <Icon icon="material-symbols:delete-sharp" />
             </button>
         </div>
-        <div>
+        <div class="mt-4">
             <Card />
         </div>
     </div>
