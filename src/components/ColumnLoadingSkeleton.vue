f68ab0a8af667f0cce4caf76b3add24d51f250f7
Sriharsh Dubale <dsriharsh@gmail.com>
2024-11-20 19:01:53 -0500

    loading skeleton added to homepage

---
+++ src/components/ColumnLoadingSkeleton.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+<template>
+    <div class="flex flex-col bg-gray-100 rounded-lg p-4 min-w-[250px] mt-4 animate-pulse w-64 p-4 space-y-4">
+        <div class="h-6 bg-gray-300 rounded w-3/4"></div>
+        <div v-for="index in 5" :key="index" class="h-4 bg-gray-300 rounded w-full"></div>
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,13 +4,18 @@ import AddColumn from '../components/AddColumn.vue'
 import { ref, onMounted, watch } from 'vue'
 import { getColumns } from "../services/columnService.js"
 import { token } from '../api/api.js'
+import ColumnLoadingSkeleton from '../components/ColumnLoadingSkeleton.vue'
 
 const columns = ref([])
+const loading = ref(true)
 
 const fetchColumns = () => {
   getColumns(9).then((res) => {
+    loading.value = false
     columns.value = res
-  }).catch(() => { })
+  }).catch(() => {
+    loading.value = false
+  })
 
 }
 
@@ -26,7 +31,9 @@ watch(token, () => {
 
 <template>
   <div class="flex gap-4 overflow-x-auto p-4">
-    <Column v-for="(column, index) in columns" :key="index" :data="column" :refreshColumns="fetchColumns" />
+    <ColumnLoadingSkeleton v-if="loading" v-for="index in 4" :key="index" />
+    <Column v-else v-for="(column, index) in columns" :key="'column-' + index" :data="column"
+      :refreshColumns="fetchColumns" />
     <AddColumn :refreshColumns="fetchColumns" />
   </div>
 </template>
