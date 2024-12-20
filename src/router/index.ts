e2e7623782626cc6e753d918d9054d18d78b9761
Sriharsh Dubale <dsriharsh@gmail.com>
2024-12-20 17:02:42 -0500

    base route redirected to boards

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,9 +3,9 @@ import HomeView from "../views/BoardView.vue";
 import LogInView from "../views/LogInView.vue";
 import BoardsView from "../views/BoardsView.vue";
 import SignupView from "../views/SignupView.vue";
-
 import { checkIfTokenExists } from "../services/authService.js";
 import DefaultLayout from "../components/layout/DefaultLayout.vue";
+import { get } from "lodash";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -43,6 +43,10 @@ const router = createRouter({
 export default router;
 
 router.beforeEach((to, from, next) => {
+  if (get(to, "fullPath") === "/") {
+    router.push("/boards");
+    return;
+  }
   if (!checkIfTokenExists() && to.meta.requiresAuth) {
     next({ name: "login" });
   } else if (!to.meta.requiresAuth && checkIfTokenExists()) {
